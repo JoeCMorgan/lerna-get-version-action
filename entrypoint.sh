b61286585a748117d47c28af7a110d0dd40b4991
Joe Morgan <joecmorgan21@gmail.com>
2021-06-25 15:41:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
+LERNA_PATH="${1-.}"
 
-LERNA_VERSION=$(grep -m1 version lerna.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+LERNA_VERSION=$(grep -m1 version ${PACKAGE_JSON_PATH}/lerna.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 echo ::set-output name=lerna-version::$LERNA_VERSION
